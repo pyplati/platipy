322cd02c13fe334716766736986e4e2d8c896e3e
Phillip Chlap <phillip.chlap@unsw.edu.au>
2022-02-15 03:04:38 +0000

    Update to backend development guide

--- platipy/backend/manage.py
+++ platipy/backend/manage.py
@@ -13,10 +13,12 @@
 # limitations under the License.
 
 import subprocess
+from multiprocessing import Process
+import uuid
 
 from sqlalchemy.exc import OperationalError
-import uuid
 import click
+import redis
 
 from .models import db, APIKey
 
@@ -92,7 +94,6 @@ def initdb(ctx):
 @cli.command()
 @click.pass_context
 @click.option("--ip", "-ip", help="IP address of service", required=True)
-@click.option("--ip", "-ip", help="IP address of service", required=True)
 def ssl(ctx, ip):
 
     command = [
@@ -120,6 +121,57 @@ def ssl(ctx, ip):
     click.echo(stderr)
 
     click.echo("SSL Certificates Created. Service restart required.")
+
+
+def run_celery():
+
+    celery_command = [
+        "celery",
+        "--app=service:celery",
+        "beat",
+        "--loglevel=INFO",
+    ]
+
+    process = subprocess.Popen(celery_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+
+    click.echo(stdout)
+    click.echo(stderr)
+
+
+@cli.command()
+@click.pass_context
+@click.option("--port", "-p", help="Port on which to run service", default=8000)
+def run(ctx, port):
+
+    r = redis.client.Redis()
+    try:
+        r.ping()
+    except redis.exceptions.ConnectionError:
+        click.echo("redis is not running. Start with command: `redis-server --daemonize yes`")
+        return
+
+    process_celery = Process(target=run_celery)
+    process_celery.start()
+
+    service_command = [
+        "gunicorn",
+        "-b",
+        f":{port}",
+        "--timeout",
+        "300",
+        "--graceful-timeout",
+        "60",
+        "service:app",
+    ]
+
+    process = subprocess.Popen(
+        f"gunicorn -b :{port} service:app",
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True,
+    )
+    process.communicate()
 
 
 if __name__ == "__main__":
