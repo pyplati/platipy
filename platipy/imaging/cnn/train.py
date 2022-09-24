b007d0f8a4a36f638e65f995465f2278bd9377c9
Phillip Chlap <phillip.chlap@unsw.edu.au>
2022-09-25 09:58:18 +1000

    Don't early stop

--- platipy/imaging/cnn/train.py
+++ platipy/imaging/cnn/train.py
@@ -163,7 +163,7 @@ class ProbUNet(pl.LightningModule):
             "optimizer": optimizer,
             "lr_scheduler": {
                 "scheduler": torch.optim.lr_scheduler.ReduceLROnPlateau(
-                    optimizer, "max", patience=5, threshold=0.01, factor=0.5
+                    optimizer, "max", patience=25, threshold=0.01, factor=0.5
                 ),
                 "monitor": "probabilisticDice",
             },
@@ -819,7 +819,7 @@ def main(args, config_json_path=None):
     early_stop_callback = EarlyStopping(
         monitor="probabilisticDice", min_delta=0.005, patience=25, verbose=False, mode="max"
     )
-    trainer.callbacks.append(early_stop_callback)
+    # trainer.callbacks.append(early_stop_callback)
 
     trainer.fit(prob_unet, data_module)
 
